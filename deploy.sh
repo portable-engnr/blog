c0157ac39bb895ae8e430fd6bc58f36527ec633f
Otsuki Shohei <otsuki.shohei@zeroidea.co.jp>
2020-09-29 13:58:04 +0900

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,16 @@
 # If a command fails then the deploy stops
 set -e
 
+# Cleaning up public file
+rm -rf /public
+
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
-cd public
+cd /public
 
 # Add changes to git.
 git add .
